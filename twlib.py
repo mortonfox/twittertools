ddd594456fbed682324b6bdfeb1b51e71167fbe9
Morton Fox <github@mortonfox.otherinbox.com>
2010-09-10 21:09:31 +0000

    move some common functions to twlib

---
+++ twlib.py
@@ -0,0 +1,152 @@
+"""
+twlib.py - Module containing some shared functions.
+"""
+
+use_simplejson = False
+try:
+    import json
+except ImportError:
+    import simplejson
+    use_simplejson = True
+
+
+def parse_json(s):
+    """
+    Parse JSON string. This will work whether we have the simplejson or the
+    json library module.
+    """
+    if use_simplejson:
+	return simplejson.loads(s)
+    else:
+	return json.loads(s)
+
+
+import oauthConsumer
+import webbrowser
+import sys
+
+OAUTH_FILENAME = 'oauth.key'
+
+CONSUMER_KEY = 'VjwGaraEThGrC81x9ZGjfQ'
+CONSUMER_SECRET = 'YlI6JRhqyxP5SFVXXbEbI3G5gO3RKGi68O5i3l7uw'
+
+REQUEST_URL = "https://api.twitter.com/oauth/request_token" 
+ACCESS_URL = "https://api.twitter.com/oauth/access_token" 
+AUTHORIZE_URL ="https://api.twitter.com/oauth/authorize" 
+
+def init_oauth(force_login=False):
+    """
+    Log in to OAuth.
+    Reads the access token and secret from a file if available.
+    Otherwise, does the required authentication.
+
+    Args:
+    	force_login
+	If True, then redo the OAuth handshake even if there is a saved
+	access token. Default is False.
+    """
+    c = oauthConsumer.Client(
+	    key = CONSUMER_KEY,
+	    secret = CONSUMER_SECRET,
+	    requestTokenURL = REQUEST_URL,
+	    accessTokenURL = ACCESS_URL, 
+	    authorizeURL = AUTHORIZE_URL, 
+	    callbackURL = "oob")
+
+    if not force_login:
+
+	# Use the saved OAuth key info if available.
+	try:
+	    oauth_file = open(OAUTH_FILENAME, "r")
+
+	    token = oauth_file.readline().rstrip()
+	    secret = oauth_file.readline().rstrip()
+
+	    oauth_file.close()
+
+	    if token != "" and secret != "":
+		c.setSession(token, secret)
+
+	except IOError:
+	    pass
+
+    # If no saved OAuth info, we need to log in.
+    if c._sessionToken is None:
+	authURL = c.requestAuth()
+	webbrowser.open(authURL)
+
+	print >> sys.stderr, "Enter PIN: ",
+	pin = raw_input()
+
+	sessionInfo = c.requestSession(c._requestToken, pin)
+
+	oauth_file = open(OAUTH_FILENAME, "w")
+	oauth_file.write(c._sessionToken + "\n")
+	oauth_file.write(c._sessionSecret + "\n")
+	oauth_file.close()
+
+    return c
+
+
+use_optparse = False
+try:
+    import argparse
+except ImportError:
+    import optparse
+    use_optparse = True
+
+class CmdlineParser:
+    def __init__(self, desc):
+	self.desc = desc
+	self.optionlist = []
+	self.paramlist = []
+
+    def add_option(self, *parg, **karg):
+	self.optionlist += [ (parg, karg) ]
+
+    def add_param(self, pname, help):
+	self.paramlist += [ (pname, help) ]
+
+    def do_parse(self):
+	if use_optparse:
+	    usagestr = 'usage: %prog [options] ' + \
+		    ' '.join([x[0] for x in self.paramlist])
+		    
+	    paramhelp = 'Positional arguments: ' + \
+		    ', '.join([x[0] + ' = ' + x[1] 
+			for x in self.paramlist])
+	    parser = optparse.OptionParser(
+		    usage = usagestr, 
+		    description = self.desc,
+		    epilog = paramhelp)
+
+	    for opt in self.optionlist:
+		parser.add_option(*opt[0], **opt[1])
+
+	    (options, args) = parser.parse_args()
+
+	    if len(args) < len(self.paramlist):
+		parser.error('too few arguments')
+
+	    for i in range(len(self.paramlist)):
+		setattr(options, self.paramlist[i][0], args[i])
+	    return options
+
+	else:
+	    parser = argparse.ArgumentParser(description = self.desc)
+
+	    for opt in self.optionlist:
+		parser.add_argument(*opt[0], **opt[1])
+
+	    for opt in self.paramlist:
+		parser.add_argument(opt[0], help=opt[1])
+
+	    args = parser.parse_args()
+
+	    return args
+
+
+if __name__ == '__main__':
+    pass
+
+# vim:set tw=0:
--- twlistcheck.py
+++ twlistcheck.py
@@ -2,94 +2,19 @@
 twlistcheck.py - Check Twitter list for protected users whom I'm not following.
 """
 
-import oauthConsumer
-import webbrowser
-
-use_simplejson = False
-try:
-    import json
-except ImportError:
-    import simplejson
-    use_simplejson = True
-
 import sys
 import time
 import pprint
+import twlib
 
-use_optparse = False
-try:
-    import argparse
-except ImportError:
-    import optparse
-    use_optparse = True
-
-OAUTH_FILENAME = 'oauth.key'
-
-CONSUMER_KEY = 'VjwGaraEThGrC81x9ZGjfQ'
-CONSUMER_SECRET = 'YlI6JRhqyxP5SFVXXbEbI3G5gO3RKGi68O5i3l7uw'
-
-def init_oauth(force_login=False):
-    """
-    Log in to OAuth.
-    Reads the access token and secret from a file if available.
-    Otherwise, does the required authentication.
-    """
-    c = oauthConsumer.Client(
-	    key=CONSUMER_KEY,
-	    secret=CONSUMER_SECRET,
-	    requestTokenURL="https://api.twitter.com/oauth/request_token", 
-	    accessTokenURL="https://api.twitter.com/oauth/access_token", 
-	    authorizeURL="https://api.twitter.com/oauth/authorize", 
-	    callbackURL="oob")
-
-    if not force_login:
-
-	# Use the saved OAuth key info if available.
-	try:
-	    oauth_file = open(OAUTH_FILENAME, "r")
-
-	    token = oauth_file.readline().rstrip()
-	    secret = oauth_file.readline().rstrip()
-
-	    oauth_file.close()
-
-	    if token != "" and secret != "":
-		c.setSession(token, secret)
-
-	except IOError:
-	    pass
-
-    # If no saved OAuth info, we need to log in.
-    if c._sessionToken is None:
-	authURL = c.requestAuth()
-	webbrowser.open(authURL)
-
-	print >> sys.stderr, "Enter PIN: ",
-	pin = raw_input()
-
-	sessionInfo = c.requestSession(c._requestToken, pin)
-
-	oauth_file = open(OAUTH_FILENAME, "w")
-	oauth_file.write(c._sessionToken + "\n")
-	oauth_file.write(c._sessionSecret + "\n")
-	oauth_file.close()
-
-    return c
-
-
-class Count:
-    count = 0
+count = 0
 
 def process_result(str):
     """
     Process the JSON output from the Twitter API call.
     Prints out the list of users.
     """
-
-    if use_simplejson:
-	jsn = simplejson.loads(str)
-    else:
-	jsn = json.loads(str)
+    jsn = twlib.parse_json(str)
 
     cursor = jsn['next_cursor']
 
@@ -97,12 +22,11 @@ def process_result(str):
 #     pp.pprint(jsn)
 
     for user in jsn['users']:
+	global count
 
 	if user['protected'] and not user['following']:
-	    Count.count += 1
-	    print "%d: %s %s" % (Count.count, 
-		    user['id'], 
-		    user['screen_name'])
+	    count += 1
+	    print "%d: %s %s" % (count, user['id'], user['screen_name'])
 
     return cursor
 
@@ -130,39 +54,16 @@ def get_pages(user, list, client):
 
 
 def main():
-    if use_optparse:
+    parser = twlib.CmdlineParser(desc='Check a Twitter list for protected users I\'m not following.')
+    parser.add_option('-l', '--login', action='store_true', 
+	    dest='login', default=False, help='Force OAuth login')
+    parser.add_param('user', help="User name")
+    parser.add_param('list', help="List name")
+    args = parser.do_parse()
+    
+    client = twlib.init_oauth(args.login)
 
-	parser = optparse.OptionParser(
-		usage = 'usage: %prog [options] user list')
-
-	parser.add_option('-l', '--login', action='store_true', 
-		dest='login', default=False, help='Force OAuth login')
-
-	(options, args) = parser.parse_args()
-
-	if len(args) < 2:
-	    parser.error('too few arguments')
-
-	client = init_oauth(options.login)
-
-	get_pages(args[0], args[1], client)
-
-    else:
-	parser = argparse.ArgumentParser(description='Dump a Twitter list.')
-
-	parser.add_argument('-l', '--login', 
-		action='store_true', 
-		default=False, 
-		help='Force OAuth login')
-
-	parser.add_argument('user', help="User name")
-	parser.add_argument('list', help="List name")
-
-	args = parser.parse_args()
-
-	client = init_oauth(args.login)
-
-	get_pages(args.user, args.list, client)
+    get_pages(args.user, args.list, client)
 
 
 if __name__ == '__main__':
